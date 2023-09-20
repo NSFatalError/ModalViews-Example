c1db36ed72b0d432584e4fe4538b13bb1b12b676
Kamil Strzelecki <strzeleckikamil.developer@gmail.com>
2023-09-20 18:35:36 +0200

    Initial Commit

---
+++ ModalViews-Example/ModalViews_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViews_ExampleApp.swift
+//  ModalViews-Example
+//
+//  Created by Kamil Strzelecki on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModalViews_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
